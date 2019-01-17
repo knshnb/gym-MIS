9e9fe54d921cd61fd42a81191cda2f8616b5a6cd
Kenshin Abe <abe.kenshin@gmail.com>
2019-01-17 18:03:06 +0900

    Support set_graph

--- gym_MIS/envs/MIS_env.py
+++ gym_MIS/envs/MIS_env.py
@@ -3,14 +3,20 @@ import numpy as np
 
 # sample graph
 # optimal solution is {0, 2, 3}
-N = 4
 SAMPLE_GRAPH = np.array([
     [0, 1, 0, 0],
     [1, 0, 1, 0],
     [0, 1, 0, 0],
     [0, 0, 0, 0],
-])
+], dtype=np.bool)
 
+"""
+graph: original graph
+A: current subgraph
+to_vertex: index of A -> index of graph
+ans: current answer for MIS
+reward: current number of vertices in answer
+"""
 class MISEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     def __init__(self):
@@ -18,10 +24,14 @@ class MISEnv(gym.Env):
         # self.action_space = N
         # self.observation_space = []
         # self.reward_range = []
+        pass
+
+    def set_graph(self, graph=SAMPLE_GRAPH):
+        self.graph = graph
         self.reset()
 
     def reset(self):
-        self.A = SAMPLE_GRAPH
+        self.A = self.graph
         self.to_vertex = np.arange(self.A.shape[0])
         self.ans = []
         self.reward = 0  # number of vertices already counted in the solution
@@ -37,7 +47,7 @@ class MISEnv(gym.Env):
         self.A = self.A[remain][:, remain]
         self.reward += 1
         assert self.A.shape[0] == 0 or self.A.shape[0] == self.A.shape[1]
-        return self.A, self.reward, self.A.shape[0] == 0, self.ans
+        return self.A, self.reward, self.A.shape[0] == 0, {'ans': self.ans}
 
     def render(self, mode='human', close=False):
         print(self.A)
